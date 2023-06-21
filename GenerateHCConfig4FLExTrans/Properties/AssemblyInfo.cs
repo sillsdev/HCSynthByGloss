2723cb4e1e97064908cef7775b86c7c1ad5cbee7
Andy Black <blackhandrew@gmail.com>
2023-06-21 13:16:45 -0700

    Version 1.6.0

--- GenerateHCConfig4FLExTrans/Properties/AssemblyInfo.cs
+++ GenerateHCConfig4FLExTrans/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SIL International")]
 [assembly: AssemblyProduct("AllomorphGenerator")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,5 +27,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
