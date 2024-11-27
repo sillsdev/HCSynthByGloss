df8894182b366f4bb158250748042883c4eb9860
Andy Black <blackhandrew@gmail.com>
2024-11-27 10:31:54 -0700

    Make HCSynthByGloss EXE use library

--- HCSynthByGlossLib/Properties/AssemblyInfo.cs
+++ HCSynthByGlossLib/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HCSynthByGlossLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for common classes for running HermitCrab synthesis based on a set of glosses")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SIL International")]
 [assembly: AssemblyProduct("HCSynthByGlossLib")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.6.4.0")]
+[assembly: AssemblyFileVersion("1.6.4.0")]
